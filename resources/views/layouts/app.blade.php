25e1b141f018a4af4044a9dd03de49dac8de1da4
Bastien SOREAU <bastien.soreau@my-digital-school.org>
2025-11-06 19:55:51 +0100

    fix(main) add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,26 +12,22 @@
   <body @php(body_class('font-manrope bg-dark-03'))>
     @php(wp_body_open())
 
-    <div id="app" class="max-w-(--max-w-1920) mx-auto">
+    <div id="app" class="flex flex-col justify-between items-start max-w-(--max-w-1920) mx-auto min-h-screen overflow-hidden">
       <a class="sr-only focus:not-sr-only" href="#main">
         {{ __('Skip to content', 'sage') }}
       </a>
 
-      @include('sections.header')
-      
-      <div class="w-full px-4 sm:px-0 sm:w-3/4 mx-auto max-w-full 1440:max-w-(--max-w-1279) 1920:max-w-(--max-w-1593)">
-        <main id="main" class="main">
-          @yield('content')
-        </main>
-  
-        @hasSection('sidebar')
-          <aside class="sidebar">
-            @yield('sidebar')
-          </aside>
-        @endif
-  
-        @include('sections.footer')
+      <div id="content" class="content w-full">
+        @include('sections.header')
+
+        <div class="w-full px-4 sm:px-0 sm:w-8/10 mx-auto max-w-full 1440:max-w-(--max-w-1279) 1920:max-w-(--max-w-1593)">
+          <main id="main" class="main">
+            @yield('content')
+          </main>
+        </div>
       </div>
+
+      @include('sections.footer')
     </div>
 
     @php(do_action('get_footer'))
--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,3 +1,38 @@
-<footer class="content-info">
-  @php(dynamic_sidebar('sidebar-footer'))
-</footer>
+@php
+  $services = [
+    'Event Photography',
+    'Commercial Photography',
+    'Product Photography',
+    'Portrait Photography',
+    'Lifestyle Photography',
+    'Wedding Photography',
+    'Landscape Photography',
+    'Branding Photography',
+    'Portrait  Photography',
+  ];
+@endphp
+
+<div class="flex flex-col items-start w-full px-4 sm:px-0 sm:w-8/10 mx-auto max-w-full 1440:max-w-(--max-w-1279) 1920:max-w-(--max-w-1593)">
+  <div class="flex flex-col items-end 1920:gap-25 1440:gap-15 gap-5 self-stretch">
+    <div class="flex flex-wrap justify-center items-center gap-2 sm:gap-3.5">
+      @svg('bastien.b', '1920:h-53.25 1440:h-42.25 h-11 w-auto')
+      @svg('bastien.a', '1920:h-53.25 1440:h-42.25 h-11 w-auto')
+      @svg('bastien.s', '1920:h-53.25 1440:h-42.25 h-11 w-auto')
+      @svg('bastien.t', '1920:h-53.25 1440:h-42.25 h-11 w-auto')
+      @svg('bastien.i', '1920:h-53.25 1440:h-42.25 h-11 w-auto')
+      @svg('bastien.e', '1920:h-53.25 1440:h-42.25 h-11 w-auto')
+      @svg('bastien.n', '1920:h-53.25 1440:h-42.25 h-11 w-auto')
+    </div>
+
+    <ul class="full-bleed-centered flex 1920:p-5 1920:gap-5 p-4 gap-4 self-stretch border-t border-b border-dark-12 bg-dark-06">
+      @foreach ($services as $service)
+        <li class="flex 1920:gap-2.5 gap-1.5 items-center">
+          <x-icon-start class="text-purple-80 1920:w-10 w-7.5 h-auto" />
+          <span class="truncate  text-purple-90 text-sm 1920:text-lg font-normal leading-150 uppercase">
+            {{ $service }}
+          </span>
+        </li>
+      @endforeach
+    </ul>
+  </div>
+</div>  
