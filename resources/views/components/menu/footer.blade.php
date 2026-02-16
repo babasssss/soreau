788c102a08aa01a8add813e7287838738c37797d
Bastien SOREAU <bastien.soreau@my-digital-school.org>
2026-02-16 21:42:21 +0100

    fix(main) : add style cf7

--- resources/views/components/menu/footer.blade.php
+++ resources/views/components/menu/footer.blade.php
@@ -7,11 +7,11 @@
           $children  = is_array($item->children ?? null) ? $item->children : [];
         @endphp
         <ul class="flex flex-col items-start 1920:gap-5 1440:gap-4 gap-3.5">
-          <p class="text-grey-50 font-semibold 1920:text-18 text-sm uppercase select-none">{{ $item->label }}</p>
+          <p class="text-grey-50 font-semibold 1920:text-lg text-sm uppercase select-none">{{ $item->label }}</p>
           <div class="flex flex-col items-start 1920:gap-1.5 gap-1">
             @foreach ($children as $child)
               <li @class([ 'group flex items-start gap-2.5 border-b border-b-dark-20 hover:border-b-purple-55 transition-[border-color] duration-300', 'text-purple-55 border-b-purple-55' => $child->active ?? false, ])>
-                <a href="{{ $child->url }}" @class([ 'py-1.5 px-0 text-grey-95 text-sm 1920:text-18 uppercase font-medium !no-underline transition-colors duration-300 group-hover:text-purple-55', 'text-purple-55' => $child->active ?? false, ]) @if(!empty($child->target)) target="{{ $child->target }}" rel="noopener noreferrer" @endif @if($child->active ?? false) aria-current="page" @endif>
+                <a href="{{ $child->url }}" @class([ 'py-1.5 px-0 text-grey-95 text-sm 1920:text-lg uppercase font-medium !no-underline transition-colors duration-300 group-hover:text-purple-55', 'text-purple-55' => $child->active ?? false, ]) @if(!empty($child->target)) target="{{ $child->target }}" rel="noopener noreferrer" @endif @if($child->active ?? false) aria-current="page" @endif>
                   {{ $child->label }}
                 </a>
               </li>
